3d12389994c7a702757c466e6f34bd34b385fbb0
David <DavidGranados@fullentregas.com.ar>
2013-05-24 09:45:36 -0300

    Button

--- EnterpriseManagement/Tablas/Fondos/Form_Comprobantes.Designer.vb
+++ EnterpriseManagement/Tablas/Fondos/Form_Comprobantes.Designer.vb
@@ -22,17 +22,29 @@ Partial Class Form_Comprobantes
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(50, 67)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form_Comprobantes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(589, 496)
+        Me.Controls.Add(Me.Button1)
         Me.MaximizeBox = False
         Me.Name = "Form_Comprobantes"
         Me.Text = "Comprobantes"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
