b853a4573ff490e5af66ea87f208fcc55134d831
David <DavidGranados@fullentregas.com.ar>
2013-06-02 20:11:06 -0300

    Segundo

--- EnterpriseManagement/Tablas/Artículos/Form_Articulos.vb
+++ EnterpriseManagement/Tablas/Artículos/Form_Articulos.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form_Articulos
 
+    Private Sub Form_Articulos_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MsgBox("hola")
+    End Sub
 End Class
